129b794172c6727d5337cc68f52048d942a1fbc1
Thomas ROMUALD <thomas.romuald@obs-mip.fr>
2025-09-15 15:06:23 +0200

    wip

--- static/js/majStation.js
+++ static/js/majStation.js
@@ -5,7 +5,7 @@ $(function() {
     console.log("lecture de l'état de la station: " + $stationID);
     let baseUrl;
     if (window.ENV.PROFILE === "prod") {
-        baseUrl = "/" + window.ENV.APP_NAME + "/static/xml/";
+        baseUrl = "/" + window.ENV.PATH_NAME + "/static/xml/";
     } else {
         baseUrl = "/static/xml/";
     }
